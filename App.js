52542e4df645916dfec573be8edef89f9ef81928
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-10 22:06:03 -0400

    use the useState hook to create an items state with some mock items

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Header from './components/Header';
+import {uuid} from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Orange juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
