3296666ae4fd6a49ab47d555298615fbfef51033
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-11 11:55:47 -0400

    add styling

--- components/AddItem.js
+++ components/AddItem.js
@@ -22,6 +22,22 @@ const AddItem = () => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  btn: {
+    backgroundColor: '#c2bad8',
+    padding: 9,
+    margin: 5,
+  },
+  btnText: {
+    color: 'darkslateblue',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
 
 export default AddItem;
