e401a86153a6fec929ee038d6ca7449730f7e970
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-10 22:14:53 -0400

    create a ListItem component that will display each item of our list, and pass it the info of each item as a prop

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 import {uuid} from 'uuidv4';
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
       <Header title="Shopping List" />
       <FlatList
         data={items}
-        renderItem={({item}) => <Text>{item.text}</Text>}
+        renderItem={({item}) => <ListItem item={item} />}
       />
     </View>
   );
