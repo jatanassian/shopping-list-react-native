2ac987a5bab2e486b398064acf67991604ab29a7
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-10 21:52:42 -0400

    create a Header component with simple style and import it in the App component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Header />
     </View>
   );
 };
