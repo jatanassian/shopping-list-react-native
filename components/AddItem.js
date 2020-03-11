76dadfebf6aedf0e44d3809a991eae71b44e1ff0
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-11 12:06:43 -0400

    implement the addItem function in an onPress event

--- components/AddItem.js
+++ components/AddItem.js
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const AddItem = () => {
+const AddItem = addItem => {
   const [text, setText] = useState('');
 
   const onChange = textValue => setText(textValue);
@@ -20,7 +20,7 @@ const AddItem = () => {
         style={StyleSheet.input}
         onChangeText={onChange}
       />
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={() => addItem(text)}>
         <Text style={styles.btnText}>
           <Icon name="plus" size={20} />
           Add Item
